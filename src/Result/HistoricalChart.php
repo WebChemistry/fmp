8689a75a8d163c7cad136f842a60513b2c09b764
MartkCz <martk494@gmail.com>
2023-04-29 13:56:41 +0200

    missing class

---
+++ src/Result/HistoricalChart.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WebChemistry\Fmp\Result;
+
+use Typertion\Php\TypeAssert;
+
+final class HistoricalChart extends FmpResult
+{
+	public const META_FIELDS = ['items'];
+
+	/**
+	 * @return HistoricalChartItem[]
+	 */
+	public function getItems(): array
+	{
+		return array_map(
+			fn (mixed $item): HistoricalChartItem => new HistoricalChartItem(TypeAssert::array($item)),
+			$this->data,
+		);
+	}
+
+
+	/**
+	 * @return string[]
+	 */
+	protected function getFieldNames(): array
+	{
+		return self::META_FIELDS;
+	}
+}
