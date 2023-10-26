7e98751be31c97012c5fed2d40e32dc0fa72b22f
MartkCz <martk494@gmail.com>
2023-10-26 15:20:10 +0200

    FmpClient::requestMultiple()

--- src/FmpClient.php
+++ src/FmpClient.php
@@ -302,6 +302,20 @@ final class FmpClient
 	/**
 	 * @template T of FmpResult
 	 * @param class-string<T> $className
+	 * @param callable(): iterable<ChildrenResponse<T>> $factory
+	 * @return MergedObjectsResponse<T>
+	 */
+	public function requestMultiple(string $className, callable $factory): MergedObjectsResponse
+	{
+		$responses = $factory();
+		$responses = is_array($responses) ? $responses : iterator_to_array($responses);
+
+		return new MergedObjectsResponse($className, $responses);
+	}
+
+	/**
+	 * @template T of FmpResult
+	 * @param class-string<T> $className
 	 * @param ChildrenResponse<T>[] $responses
 	 * @return MergedObjectsResponse<T>
 	 */
